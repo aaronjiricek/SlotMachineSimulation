41536f73691605603c42e2a84b1eacc567ca2751
Aaron Jiricek <developer@bubbaseven.com>
2020-11-12 13:35:38 -0800

    removed all options and excess code.

--- slot_machine.py
+++ slot_machine.py
@@ -5,8 +5,7 @@ from random import seed
 from random import randint
 
 
-# option variables
-debug = False
+# variables
 startingCash = 100
 goal = 500
 numberOfExperiments = 1000000
@@ -17,7 +16,6 @@ maxNumberOfBets = 21
  
 def main():
     seed()
-    GetOptions()
     RunGames()
  
 def RunGames():
@@ -39,8 +37,6 @@ def RunGames():
             winings = 0
             for spins in range(numberOfBets):
                 winings += spin(bet)
-                if (debug):
-                    print (f"experiments={experiments}, spins={spins}, winings={winings}")
                 if(winings >= (goal * 100)):
                     goBigCount += 1
                     break
@@ -49,8 +45,6 @@ def RunGames():
         probabilityOfGoBig = goBigCount / numberOfExperiments
 
         # print the results for this experiment
-        if(debug):
-            print(f"goBigCount={goBigCount}, numberOfExperiments={numberOfExperiments}")
         print(f"{probabilityOfGoBig}", flush=True)
 
 
@@ -68,59 +62,6 @@ def spin(_bet):
     else:
         return 30*_bet
 
-# get options for running the program
-def GetOptions():
-    global debug
-    global startingCash
-    global goal
-    global numberOfExperiments
-    global minNumberOfBets
-    global maxNumberOfBets
- 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd", ["help", "debug", "cash=", "goal=", "minbets=", "maxbets=", "experiments="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(err) # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-    output = None
-    for o, a in opts:
-        if o in ("-d", "--debug"):
-            print("got here")
-            debug = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o == "--cash":
-            startingCash = int(a)
-        elif o == "--goal":
-            goal = int(a)
-        elif o == "--minbets":
-            minNumberOfBets = int(a)
-        elif o == "--maxbets":
-            maxNumberOfBets = int(a)
-        elif o == "--experiments":
-            numberOfExperiments = int(a)
-        else:
-            assert False, "unhandled option"
-  
-# help menu
-def usage():
-    usageString = """slot machine.py simulates running a slot machine for Amanda's class.
- 
-This will simulate a large number of experiments and output the percentage of those experiments where the player won more than --goal amount of money.
- 
-Options:
-    -h --help     Print this help file
-    -d --debug    Print debug information
-    --cash        Starting Cash.  Default 100
-    --goal        Amount of Cash at the end of each experiment to be considered sucessful.  Default 500
-    --minbets     Min number of Bets to make
-    --maxbets     Max number of Bets to make
-    --experiments Number of Experiments to run.  Default 1,000,000
-"""
-    print(usageString)
  
 if __name__ == "__main__":
     main()
